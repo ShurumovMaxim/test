87e468e64dc70aead8e327f305984d223714aafe
ShurumovMaxim <work6921@yandex.ru>
2016-05-21 11:09:23 +0700

    Update mainKY.c

--- mainKY.c
+++ mainKY.c
@@ -3,10 +3,10 @@
 void KY(int a, int b, int c, float *x1, float *x2);
 
 int main() {
-  int a,b,c;
+  int a = 1, b = 4, c = 4;
   float x1, x2;
   printf("Vvedite koeficienti");
-  scanf("%d %d %d",&a,&b,&c);
+//  scanf("%d %d %d",&a,&b,&c);
   KY(a, b, c, &x1, &x2);
   return 0;
 }
